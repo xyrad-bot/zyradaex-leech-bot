0503732cbfe8dec72a80502d5217583d009e9a78
clyfly <deni@xyradcloud.my.id>
2024-07-09 10:05:14 +0700

    minor changes
    Signed-off-by: clyfly <deni@xyradcloud.my.id>

--- bot/helper/mirror_leech_utils/gdrive_utils/helper.py
+++ bot/helper/mirror_leech_utils/gdrive_utils/helper.py
@@ -197,7 +197,7 @@ class GoogleDriveHelper:
     def create_directory(self, directory_name, dest_id):
         file_metadata = {
             "name": directory_name,
-            "description": "Uploaded by Mirror-leech-telegram-bot",
+            "description": "Uploaded by Meiyonghua-Mltb",
             "mimeType": self.G_DRIVE_DIR_MIME_TYPE,
         }
         if dest_id is not None:
--- bot/helper/mirror_leech_utils/gdrive_utils/upload.py
+++ bot/helper/mirror_leech_utils/gdrive_utils/upload.py
@@ -146,7 +146,7 @@ class gdUpload(GoogleDriveHelper):
         # File body description
         file_metadata = {
             "name": file_name,
-            "description": "Uploaded by Mirror-leech-telegram-bot",
+            "description": "Uploaded by Meiyonghua-Mltb",
             "mimeType": mime_type,
         }
         if dest_id is not None:
